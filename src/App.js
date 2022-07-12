58093caf2b163f0ab59f3d4ae18685477b0c3b26
jsk3342 <kss2460@naver.com>
2022-07-12 10:30:57 +0900

    Add: nav바 추가 및 Normalize 설정

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Nav from "./component/Nav";
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Signup from "./pages/signup/Signup";
@@ -7,10 +8,11 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Nav />
         <Routes>
-          <Route path='/' element={<Home/>}></Route>
-          <Route path='/login' element={<Login/>}></Route>
-          <Route path='/signup' element={<Signup/>}></Route>
+          <Route path='/' element={<Home />}></Route>
+          <Route path='/login' element={<Login />}></Route>
+          <Route path='/signup' element={<Signup />}></Route>
         </Routes>
       </BrowserRouter>
     </div>
